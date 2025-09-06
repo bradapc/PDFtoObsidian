3ddec35a306e51d6b702ec5f33f954a4eb0ad90e
bradapc <brad.cocar@gmail.com>
2025-09-06 13:39:02 -0700

    Added pdf selection, renaming and output of .md file based on file selection path

--- slides.py
+++ slides.py
@@ -1,11 +1,13 @@
 from pdf2image import convert_from_path
+from tkinter import filedialog as fd
+import tkinter as tk
+import os
+from pathlib import Path
 
 def convert_pdf_to_png(pdf_path):
     try:
-        images = convert_from_path(pdf_path, dpi=100, poppler_path=r"C:\Program Files (x86)\poppler-25.07.0\Library\bin")
-        pdf_name=pdf_path.split('.')[0]
-        output_folder=f"{pdf_name}_Slides"
-        import os
+        images = convert_from_path(f"{pdf_path}.pdf", dpi=100, poppler_path=r"C:\Program Files (x86)\poppler-25.07.0\Library\bin")
+        output_folder=f"{pdf_path}_Slides"
         os.makedirs(output_folder, exist_ok=True)
 
         for i, image in enumerate(images):
@@ -17,9 +19,10 @@ def convert_pdf_to_png(pdf_path):
 
 def output_obsidian_markdown(slides_path, pdf_name):
     try:
-        import os
         slides = sorted(os.listdir(slides_path))
-        output_file = open(f"{pdf_name}.md", "a")
+        initial_path = Path(slides_path)
+        output_path = initial_path.parent / f"{pdf_name}.md"
+        output_file = open(output_path, "a")
         print(f"Created output file {pdf_name}.md")
         for slide in slides:
             if slide.endswith(".png") or slide.endswith(".jpg"):
@@ -28,5 +31,27 @@ def output_obsidian_markdown(slides_path, pdf_name):
     except Exception as e:
         print(f"Error reading and outputting markdown {e}")
 
-convert_pdf_to_png("Lecture 2 - Intro to Networks.pdf")
-output_obsidian_markdown("Lecture 2 - Intro to Networks_Slides", "Lecture 2 - Intro to Networks")
+def main():
+    root = tk.Tk()
+    root.withdraw()
+    file_path = fd.askopenfilename(
+        title="Select a PDF",
+    )
+    if file_path:
+        if (not file_path.endswith(".pdf")):
+            print("File must be a PDF. Aborting...")
+            exit(1)
+        file_name = os.path.basename(file_path)
+        markdown_name = file_name.split(".")[0]
+        file_path = file_path.split(".")[0]
+        print(f"The name of your markdown file will be {markdown_name}")
+        user_input = input("Press <Enter> to confirm or enter a new name: ")
+        markdown_name = markdown_name if not user_input else user_input
+        convert_pdf_to_png(file_path)
+        output_obsidian_markdown(f"{file_path}_Slides", markdown_name)
+    else:
+        print("No file selected. Aborting...")
+        exit(2)
+
+if __name__ == "__main__":
+    main()

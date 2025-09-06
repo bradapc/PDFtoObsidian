486cbe40687890343db7830e84151912b4e2e4f9
bradapc <brad.cocar@gmail.com>
2025-09-06 13:09:53 -0700

    Added .md output of slide images to create skeleton of lecture notes file.

--- slides.py
+++ slides.py
@@ -1,16 +1,32 @@
 from pdf2image import convert_from_path
 
-def convert_pdf_to_png(pdf_path, output_folder="output_images"):
+def convert_pdf_to_png(pdf_path):
     try:
         images = convert_from_path(pdf_path, dpi=100, poppler_path=r"C:\Program Files (x86)\poppler-25.07.0\Library\bin")
+        pdf_name=pdf_path.split('.')[0]
+        output_folder=f"{pdf_name}_Slides"
         import os
         os.makedirs(output_folder, exist_ok=True)
 
         for i, image in enumerate(images):
-            output_filename = os.path.join(output_folder, f"page_{i+1}.png")
+            output_filename = os.path.join(output_folder, f"slide_{i+1}.png")
             image.save(output_filename, "PNG")
             print(f"Saved: {output_filename}")
     except Exception as e:
         print(f"Error converting PDF: {e}")
 
-convert_pdf_to_png("test.pdf")
+def output_obsidian_markdown(slides_path, pdf_name):
+    try:
+        import os
+        slides = sorted(os.listdir(slides_path))
+        output_file = open(f"{pdf_name}.md", "a")
+        print(f"Created output file {pdf_name}.md")
+        for slide in slides:
+            if slide.endswith(".png") or slide.endswith(".jpg"):
+                output_file.write(f"![[{slides_path}/{slide}]]\n\n- Notes:\n\n---\n")
+        print(f"Output finished. Please use the {pdf_name}.md file for your notes")
+    except Exception as e:
+        print(f"Error reading and outputting markdown {e}")
+
+convert_pdf_to_png("Lecture 2 - Intro to Networks.pdf")
+output_obsidian_markdown("Lecture 2 - Intro to Networks_Slides", "Lecture 2 - Intro to Networks")
